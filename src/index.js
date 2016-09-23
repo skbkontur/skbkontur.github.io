9ee7405028d1280c9d579e399da28cc2fdef2c91
tihonove <tihonove@skbkontur.ru>
2016-09-23 18:40:14 +0500

    totally update markup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-import './index.less'
+import './styles/index.less'
 
+document.getElementById('site-navigation-button').onclick = function() {
+    document.getElementById('site-navigation-button').classList.toggle("opened");
+    document.getElementById('nav-bar').classList.toggle("show-menu");
+    document.getElementsByTagName('html')[0].classList.toggle("overlay");
+}
