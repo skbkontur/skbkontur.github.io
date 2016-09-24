403cc842d3ce5e1801a2935ad6ff8206eb680552
tihonove <tihonove@skbkontur.ru>
2016-09-24 15:40:49 +0500

    allow to close menu with swipe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import './styles/index.less'
+import Hammer from 'hammerjs';
+
+const siteNavigation = document.getElementById('site-navigation');
+let containerWidth = null;
 
 function toggleMenu() {
     document.getElementById('site-navigation-button').classList.toggle("opened");
     document.getElementById('nav-bar').classList.toggle("show-menu");
     document.getElementsByTagName('html')[0].classList.toggle("overlay");
+    setTimeout(function() {
+        containerWidth = siteNavigation.offsetWidth;
+        siteNavigation.style.width = 'auto';
+    }, 500);
+
 }
 
 document.getElementById('site-navigation-button').onclick = toggleMenu;
-
 document.getElementById('site-navigation-menu-overlay').onclick = toggleMenu;
+
+var hammer = new Hammer(document.getElementById('site-navigation'));
+hammer.add(new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 10 }));
+hammer.on('panstart panmove panend pancancel', (ev) => {
+    var delta = ev.deltaX;
+    var percent = (100 / containerWidth) * delta;
+    siteNavigation.style.width = Math.round(containerWidth - delta).toString() + 'px';
+    if (ev.type == 'panend' || ev.type == 'pancancel') {
+        if (Math.abs(percent) > 20 && ev.type == 'panend') {
+            toggleMenu();
+        }
+    }
+});
