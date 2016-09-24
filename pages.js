1f2ce931b8fd6faa9660cca1555781429a241d62
tihonove <tihonove@skbkontur.ru>
2016-09-24 18:39:46 +0500

    change language routring

--- pages.js
+++ pages.js
@@ -4,15 +4,27 @@ module.exports = [
         target: 'index.html',
     },
     {
+        template: 'index/index.ru.html',
+        target: 'ru/index.html',
+    },
+    {
         template: 'index/index.en.html',
-        target: 'index.en.html',
+        target: 'en/index.html',
     },
     {
         template: 'talks.html',
         target: 'talks.html',
     },
     {
+        template: 'talks.html',
+        target: 'ru/talks.html',
+    },
+    {
         template: 'articles.html',
         target: 'articles.html',
+    },
+    {
+        template: 'articles.html',
+        target: 'ru/articles.html',
     },
 ];
