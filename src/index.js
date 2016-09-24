7e7c56efa97b18494e164818f199e509b565fa1f
tihonove <tihonove@skbkontur.ru>
2016-09-24 15:06:41 +0500

    close menu on overlay click

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import './styles/index.less'
 
-document.getElementById('site-navigation-button').onclick = function() {
+function toggleMenu() {
     document.getElementById('site-navigation-button').classList.toggle("opened");
     document.getElementById('nav-bar').classList.toggle("show-menu");
     document.getElementsByTagName('html')[0].classList.toggle("overlay");
 }
+
+document.getElementById('site-navigation-button').onclick = toggleMenu;
+
+document.getElementById('site-navigation-menu-overlay').onclick = toggleMenu;
