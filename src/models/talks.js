5ad878d0b19786197aa39313aa0c47faa17a1598
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2016-10-07 18:51:27 +0500

    add 5 talks

--- src/models/talks.js
+++ src/models/talks.js
@@ -1,4 +1,7 @@
-import relayImage from './images/relay.png';
+import volokitinImage from './images/volokitin.jpg';
+import kazakovImage from './image/kazakov.jpg';
+import chevdarImage from './image/chevdar.jpg';
+import tihonovImage from './image/tihonov.jpg';
 
 export default {
     title: 'Выступления',
@@ -9,14 +12,54 @@ export default {
     ],
     items: [
         {
-            image: relayImage,
-            speakerFullName: 'Волокитин Павел',
-            eventName: 'FrontendFellows #1',
+            image: volokitinImage,
+            speakerFullName: 'Павел Волокитин',
+            eventName: 'FrontendFellows',
             title: 'Relay или не Relay',
-            dateString: '29.01.2016',
-            tags: ['frontend'],
+            dateString: 'январь 2016',
+            tags: ['frontend', 'js', 'react'],
             linkToVideo: 'https://www.youtube.com/watch?v=v5Fi-djuwZk',
             linkToSlides: 'http://www.slideshare.net/PavelVolokitin/frontfellows-relay-66758880',
+        },
+        {
+            image: kazakovImage,
+            speakerFullName: 'Александр Казаков',
+            eventName: 'Codefest',
+            title: 'Нетривиальное о репликации: как добиться высокой доступности',
+            dateString: 'март 2016',
+            tags: ['backend', 'operations'],
+            linkToVideo: 'https://www.youtube.com/watch?v=LFpJGGs-kuo',
+            linkToSlides: 'http://www.slideshare.net/codefest/ss-60114851',
+        },
+        {
+            image: chevdarImage,
+            speakerFullName: 'Игорь Чевдарь',
+            eventName: 'DotNext',
+            title: 'Кодогенерация для оптимизации',
+            dateString: 'июнь 2016',
+            tags: ['backend', '.net'],
+            linkToVideo: 'https://www.youtube.com/watch?v=X35JVTJV1CE&list=PLtWrKx3nUGBeYn-pSCjEgBPL90SvpylZL&index=7',
+            linkToSlides: 'http://public.jugru.org/dotnext/2016/spb/day_1/track_2/chevdar.pdf',
+        },
+        {
+            image: volokitinImage,
+            speakerFullName: 'Павел Волокитин',
+            eventName: 'WebStandartsDays',
+            title: 'Меняем JavaScript с помощью JavaScript',
+            dateString: 'июнь 2016',
+            tags: ['frontend', 'js'],
+            linkToVideo: 'https://www.youtube.com/watch?v=zoq95j3V43g',
+            linkToSlides: 'http://www.slideshare.net/PavelVolokitin/javascript-javascript-66759016',
+        },
+        {
+            image: tihonovImage,
+            speakerFullName: 'Евгений Тихонов',
+            eventName: 'WebStandartsDays',
+            title: 'Готовим бизнес-лапшу на React и Redux',
+            dateString: 'июнь 2016',
+            tags: ['frontend', 'js', 'react'],
+            linkToVideo: 'https://www.youtube.com/watch?v=OXsxo4gkqtQ',
+            linkToSlides: 'https://reelm.github.io/wsd-2016-slides/wsd-2016-slides.zip',
         },
     ],
 };
