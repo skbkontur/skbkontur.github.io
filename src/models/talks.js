036fee6e1df4dcef90645a4edfac43377061b672
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2016-10-10 19:58:40 +0500

    add 5 talks

--- src/models/talks.js
+++ src/models/talks.js
@@ -2,6 +2,10 @@ import volokitinImage from './images/volokitin.jpg';
 import kazakovImage from './images/kazakov.jpg';
 import chevdarImage from './images/chevdar.jpg';
 import tihonovImage from './images/tihonov.jpg';
+import burmistrovImage from './images/burmistrov.jpg';
+import egorovImage from './images/egorov.jpg';
+import larkovImage from './images/larkov.jpg';
+import ronzhinaImage from './images/ronzhina.png';
 
 export default {
     title: 'Выступления',
@@ -60,6 +64,56 @@ export default {
             tags: ['frontend', 'js', 'react'],
             linkToVideo: 'https://www.youtube.com/watch?v=OXsxo4gkqtQ',
             linkToSlides: 'https://reelm.github.io/wsd-2016-slides/wsd-2016-slides.zip',
+        },
+        {
+            image: burmistrovImage,
+            speakerFullName: 'Иван Бурмистров',
+            eventName: 'Cassandra Summit',
+            title: 'Digging Cassandra cluster',
+            dateString: 'сентябрь 2015',
+            tags: ['backend', 'db', 'cassandra'],
+            linkToVideo: 'https://www.youtube.com/watch?v=t6utdWssBOM',
+            linkToSlides: 'https://drive.google.com/open?id=0B-qtqQ5D71UQX1FvSlgtaUtscFE',
+        },
+        {
+            image: egorovImage,
+            speakerFullName: 'Павел Егоров',
+            eventName: 'SECR',
+            title: 'Почему вам стоит организовать свою ИТ-конференцию и как это сделать',
+            dateString: 'октябрь 2015',
+            tags: ['management', 'education'],
+            linkToVideo: 'http://0x1.tv/20151022BG',
+            linkToSlides: 'http://0x1.tv/20151022BG',
+        },
+        {
+            image: kazakovImage,
+            speakerFullName: 'Александр Казаков',
+            eventName: 'DUMP',
+            title: 'Профилирование распределенных систем',
+            dateString: 'апрель 2016',
+            tags: ['backend', 'operations'],
+            linkToVideo: 'https://www.youtube.com/watch?v=EpDgUZWjGLc&index=6&list=PLRdS-n5seLRriePAvTXvkl8mE20gPeDIp',
+            linkToSlides: 'https://drive.google.com/open?id=0B-qtqQ5D71UQY1Bpdi1PTndhOXc',
+        },
+        {
+            image: larkovImage,
+            speakerFullName: 'Алексей Ларьков',
+            eventName: 'DUMP',
+            title: 'Moira: Realtime alerting',
+            dateString: 'апрель 2016',
+            tags: ['backend', 'operations'],
+            linkToVideo: 'https://www.youtube.com/watch?v=_MJdtHbiXPE&index=5&list=PLRdS-n5seLRriePAvTXvkl8mE20gPeDIp',
+            linkToSlides: 'https://drive.google.com/file/d/0B-qtqQ5D71UQQVcwUEpybmpIZVE/view?usp=drivesdk',
+        },
+        {
+            image: ronzhinaImage,
+            speakerFullName: 'Анастасия Ронжина',
+            eventName: 'DUMP',
+            title: 'Cообщество тестировщиков Екатеринбурга',
+            dateString: 'апрель 2016',
+            tags: ['testing'],
+            linkToVideo: 'https://www.youtube.com/watch?v=-4zAc7kWAhc&index=5&list=PLRdS-n5seLRoMRdNO1k1FeFeBMgBRRX6j',
+            linkToSlides: 'https://drive.google.com/open?id=0B-qtqQ5D71UQRmF5dHVuWU8tYlU',
         },
     ],
 };
